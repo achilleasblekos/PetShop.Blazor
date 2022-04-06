a4ae29bfce554f3432153fe3ae41d00e139fdaa3
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-04-06 23:04:26 +0300

    EmployeeList & viewmodel

--- PetShop/PetShop.Blazor/Client/Pages/EmployeeList.razor.cs
+++ PetShop/PetShop.Blazor/Client/Pages/EmployeeList.razor.cs
@@ -1,4 +1,71 @@
-﻿namespace PetShop.Blazor.Client.Pages {
-    public partial class EmployeeList {
-    }
+﻿using Microsoft.AspNetCore.Components;
+using PetShop.Blazor.Shared;
+using PetShop.Model;
+using System.Net.Http.Json;
+
+namespace PetShop.Blazor.Client.Pages {
+	public partial class EmployeeList {
+		private string NewItemText { get; set; }
+		List<EmployeeListViewModel> employees = null;
+		bool isLoading = true;
+		//{
+		//new EmployeesViewModel() { Name ="Takis",Surname="Manageridis",EmployeeType=EmployeeType.Manager, SallaryPerMonth=2000m},
+		//new EmployeesViewModel() { Name = "Akis", Surname = "Staffikis", EmployeeType = EmployeeType.Staff, SallaryPerMonth = 450.60m },
+
+		protected override async Task OnInitializedAsync() { // is called when the component is rendered
+			await LoadItemsFromServer();
+			isLoading = false;
+		}
+
+		private async Task LoadItemsFromServer() {
+			employees = await httpClient.GetFromJsonAsync<List<EmployeeListViewModel>>("employees"); //get the list to route "employees"
+		}
+
+		async Task AddItem() {
+			if (string.IsNullOrWhiteSpace(NewItemText)) return;
+
+			var newEmployee = new EmployeeListViewModel { //create new employee on server
+				Name = NewItemText
+			};
+			//NewItemText = string.Empty;
+			NewItemText = null;
+
+			await httpClient.PostAsJsonAsync("employees", newEmployee); // send the new item to "employee route" //id=0?
+			await LoadItemsFromServer(); // bring back the list (also gets the newly  created id)
+		}
+
+		async Task DeleteItem(EmployeeListViewModel itemtoDelete) {
+			var confirm = await jsRuntime.InvokeAsync<bool>("confirmDelete", null);
+			if (confirm) {
+				var response = await httpClient.DeleteAsync($"employees/{itemtoDelete}");
+				response.EnsureSuccessStatusCode();
+				await LoadItemsFromServer();
+			}
+		}
+
+		async Task NameChanged(ChangeEventArgs e, EmployeeListViewModel itemtoUpdate) {
+			itemtoUpdate.Name = e.Value?.ToString();
+			UpdateLogic(e, itemtoUpdate);
+		}
+
+		async Task SurnameChanged(ChangeEventArgs e, EmployeeListViewModel itemtoUpdate) {
+			itemtoUpdate.Surname = e.Value?.ToString();
+			UpdateLogic(e, itemtoUpdate);
+		}
+
+		//async Task TypeChanged(ChangeEventArgs e, EmployeesViewModel itemtoUpdate) { // TODO: update employeeType
+		//	itemtoUpdate.EmployeeType = 
+		//	UpdateLogic(e, itemtoUpdate);
+		//}
+		async Task SalaryChanged(ChangeEventArgs e, EmployeeListViewModel itemtoUpdate) { // TODO: parse salary
+																						  //itemtoUpdate.SallaryPerMonth = e.Value.;
+			UpdateLogic(e, itemtoUpdate);
+		}
+		async void UpdateLogic(ChangeEventArgs e, EmployeeListViewModel itemtoUpdate) {
+			var response = await httpClient.PutAsJsonAsync("employees", itemtoUpdate);
+			response.EnsureSuccessStatusCode();
+			await LoadItemsFromServer();
+		}
+	}
 }
+
---
+++ PetShop/PetShop.Blazor/Shared/EmployeeListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using PetShop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Blazor.Shared {
+    public class EmployeeListViewModel {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+        public decimal SallaryPerMonth { get; set; }
+    }
+}
