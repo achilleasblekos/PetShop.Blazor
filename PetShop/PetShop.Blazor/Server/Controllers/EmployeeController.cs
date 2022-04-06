01a2a75b022b8975b1d3f5bb1f34cb05b5d13f30
GeorgeDoc <64958579+GeorgeDoc@users.noreply.github.com>
2022-04-06 23:16:46 +0300

    employee controller

---
+++ PetShop/PetShop.Blazor/Server/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetShop.Blazor.Shared;
+using PetShop.EF.Repositories;
+using PetShop.Model;
+
+namespace PetShop.Blazor.Server.Controllers {
+
+    [ApiController]
+    [Route("Controller")]
+    public class EmployeeController : ControllerBase {
+
+        private readonly IEntityRepo<Employee> _employeeRepo;
+        public EmployeeController(IEntityRepo<Employee> employeeRepo) {
+            _employeeRepo = employeeRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<EmployeeListViewModel>> Get() { //GetAll
+            var result = await _employeeRepo.GetAllAsync();
+            return result.Select(x => new EmployeeListViewModel { // use Select to convert them to ViewModel
+                ID = x.ID,
+                Name = x.Name,
+                Surname = x.Surname,
+                EmployeeType = x.EmployeeType,
+                SallaryPerMonth = x.SallaryPerMonth
+            });
+        }
+
+        [HttpPost] //[HttpPost("newRoute")] custom route if I want
+        public async Task Post(EmployeeListViewModel employee) { //create // 404
+            var newEmployee = new Employee();
+            await _employeeRepo.AddAsync(newEmployee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id) {
+            await _employeeRepo.DeleteAsync(id);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(EmployeeListViewModel employee) {
+            var itemToUpdate = await _employeeRepo.GetByIdAsync(employee.ID);
+            if (itemToUpdate == null) return NotFound();
+
+            itemToUpdate.Name = employee.Name;
+            itemToUpdate.Surname = employee.Surname;
+            itemToUpdate.EmployeeType = employee.EmployeeType;
+            itemToUpdate.SallaryPerMonth = employee.SallaryPerMonth;
+
+            await _employeeRepo.UpdateAsync(employee.ID, itemToUpdate);
+            return Ok();
+        }
+    }
+}
